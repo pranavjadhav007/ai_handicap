c48a3af5e9240165f8d7a7c50d194f1ba4d3efc0
Pranav Jadhav <88050656+pranavjadhav007@users.noreply.github.com>
2023-12-14 12:45:55 +0530

    Add files via upload

---
+++ check_v.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 14 01:42:12 2023
+
+@author: prana
+"""
+
+import cv2 as cv
+import tensorflow as tf
+import numpy as np
+
+print("OpenCV version:", cv.__version__)
+print("Numpy version:", np.__version__)
+print("tf version:", tf.__version__)
+
+
